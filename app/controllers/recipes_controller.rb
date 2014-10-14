87d61d588cf6427b81d5fb57dbbb5de8394ee1b5
Jay Wengrow <jaywngrw@gmail.com>
2014-10-14 12:58:51 -0500

    added index and show actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,9 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def show
+    @recipe = Recipe.find_by(:id => params[:id])
+    # @recipe = Recipe.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'recipes#index'
+  # root 'recipes#index'
+  get 'recipes' => 'recipes#index'
+  get 'recipes/:id' => 'recipes#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
